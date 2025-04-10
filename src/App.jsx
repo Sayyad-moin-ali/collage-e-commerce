62b6ae0241778ee277dfb5a9c5cf661f53ca2bfa
rishabh rajput <rajputrishabh164>
2025-04-10 17:28:09 +0530

    Updated App.jsx with routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import UserLayout from './components/Layout/UserLayout';
-import Home from './components/pages/Home';
+import React from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import UserLayout from './components/Layout/UserLayout'
+import Home from './components/pages/Home'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<UserLayout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path='/' element={<UserLayout/>}>
+        < Route index element={<Home/>}/>
+      </Route>
+      <Route> {/*Andmin Layout*/}  </Route>
+    </Routes>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
