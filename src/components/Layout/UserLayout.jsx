5ff7951f469f441726d9e3dd776048ddcc126c40
rishabh rajput <rajputrishabh164>
2025-04-10 19:08:14 +0530

    Updated UserLayout component

--- src/components/Layout/UserLayout.jsx
+++ src/components/Layout/UserLayout.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Header from '../Common/Header'
+import React from 'react';
+import Header from '../Common/Header';
 import Footer from '../Common/Footer';
 import { Outlet } from 'react-router-dom';
+
 const UserLayout = () => {
   return (
     <>
-    {/* Header section */}
-    <Header/>
-    {/* Main section */}
-    <main>
-      
-    </main>
-    {/* Footer section */}  
-    <Footer/>
+      {/* Header section */}
+      <Header />
+      {/* Main section */}
+      <main>
+        <Outlet /> {/* This renders the child routes */}
+      </main>
+      {/* Footer section */}
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default UserLayout
+export default UserLayout;
