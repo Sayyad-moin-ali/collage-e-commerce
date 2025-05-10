ddd500cbbdffcd0df0926ea8d2a7c78a1cba998e
sayyad-moin-ali <alisayyadmoin@gmail.com>
2025-05-10 07:20:36 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import UserLayout from './components/Layout/UserLayout'
 
+
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<UserLayout/>}>
-        < Route index element={<Home/>}/>
+        
       </Route>
       <Route> {/*Andmin Layout*/}  </Route>
     </Routes>
